0113a68749c8517a261138bb6f17588a65fc908b
Siistema <siistema@bk.ru>
2018-11-27 23:15:57 +0300

    first commit

--- 01-linear-program/main.cpp
+++ 01-linear-program/main.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main()
 {
 	double a, b;
-	double z1, z2;
 	cout << "enter a:";
 	cin >> a;
 	cout << "enter b:";
 	cin >> b;
-	z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
-	z2 = (1 + sin(2 * b)) / (cos(2 * b));
+	double z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
+	double z2 = (1 + sin(2 * b)) / (cos(2 * b));
 		cout << "z1 = " << z1 << "\nz2 = " << z2;
 	return 0;
 }
